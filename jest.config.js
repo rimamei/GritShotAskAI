0a775a998ac4ac53145d5be0822bd99750ecc7c9
Rima <rimameih@gmail.com>
2025-09-28 21:31:00 +0700

    chore: test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  // Use jsdom to simulate a browser environment
+  testEnvironment: 'jsdom',
+  
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+};
